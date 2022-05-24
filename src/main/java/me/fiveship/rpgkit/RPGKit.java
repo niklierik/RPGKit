b0cbbf1a9b3de0c96d73f04492c433c9c5bae0ab
NikliErik <erik200203@gmail.com>
2022-05-24 22:48:43 +0200

    Initial commit

---
+++ src/main/java/me/fiveship/rpgkit/RPGKit.java
@@ -0,0 +1,14 @@
+package me.fiveship.rpgkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPGKit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
